ebe46ca2c7dcee5588d50859568b003f9ac68e14
zohaibA <zohaib.ali@isis-papyrus.com>
2021-09-22 17:04:12 +0200

    change port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
+const post = process.env.POST || 3000
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -27,4 +28,4 @@ io.on('connection', socket => {
     })
 })
 
-server.listen(3000)
+server.listen(post)
